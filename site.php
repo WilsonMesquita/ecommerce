06e93b785d4cf34193ba26af52bd6d90f199819d
Wilson Mesquita <wylllson@gmail.com>
2018-08-16 21:56:09 -0300

    Carrinho produtos

--- site.php
+++ site.php
@@ -55,30 +55,34 @@ $app->get("/products/:desurl", function($desurl){
 
 
 $app->get("/cart", function(){
+
 	$cart = Cart::getFromSession();
+
 	$page = new Page();
-	$page->setTpl("cart");
-	/*$page->setTpl("cart", [
+	$page->setTpl("cart", [
 		'cart'=>$cart->getValues(),
-		'products'=>$cart->getProducts(),
-		'error'=>Cart::getMsgError()
-	]);*/
+		'products'=>$cart->getProducts()
+		//'error'=>Cart::getMsgError()
+	]);
 });
 
 
-/*$app->get("/cart/:idproduct/add", function($idproduct){
+$app->get("/cart/:idproduct/add", function($idproduct){
+	
 	$product = new Product();
 	$product->get((int)$idproduct);
 	$cart = Cart::getFromSession();
 	$qtd = (isset($_GET['qtd'])) ? (int)$_GET['qtd'] : 1;
-	for ($i = 0; $i < $qtd; $i++) {
-		
+	
+	for ($i = 0; $i < $qtd; $i++) {		
 		$cart->addProduct($product);
 	}
 	header("Location: /cart");
 	exit;
 });
+
 $app->get("/cart/:idproduct/minus", function($idproduct){
+	
 	$product = new Product();
 	$product->get((int)$idproduct);
 	$cart = Cart::getFromSession();
@@ -86,6 +90,7 @@ $app->get("/cart/:idproduct/minus", function($idproduct){
 	header("Location: /cart");
 	exit;
 });
+
 $app->get("/cart/:idproduct/remove", function($idproduct){
 	$product = new Product();
 	$product->get((int)$idproduct);
@@ -94,7 +99,8 @@ $app->get("/cart/:idproduct/remove", function($idproduct){
 	header("Location: /cart");
 	exit;
 });
-$app->post("/cart/freight", function(){
+
+/*$app->post("/cart/freight", function(){
 	$cart = Cart::getFromSession();
 	$cart->setFreight($_POST['zipcode']);
 	header("Location: /cart");
--- vendor/hcodebr/php-classes/src/model/Cart.php
+++ vendor/hcodebr/php-classes/src/model/Cart.php
@@ -95,12 +95,16 @@ class Cart extends Model {
         $sql = new Sql();
         
 		if ($all) {
+
 			$sql->query("UPDATE tb_cartsproducts SET dtremoved = NOW() WHERE idcart = :idcart AND idproduct = :idproduct AND dtremoved IS NULL", [
 				':idcart'=>$this->getidcart(),
 				':idproduct'=>$product->getidproduct()
+
 			]);
 		} else {
-			$sql->query("UPDATE tb_cartsproducts SET dtremoved = NOW() WHERE idcart = :idcart AND idproduct = :idproduct AND dtremoved IS NULL LIMIT 1", [
+			$sql->query("UPDATE tb_cartsproducts SET dtremoved = NOW() WHERE idcart = :idcart
+				AND idproduct = :idproduct
+				AND dtremoved IS NULL LIMIT 1", [
 				':idcart'=>$this->getidcart(),
 				':idproduct'=>$product->getidproduct()
 			]);
@@ -111,16 +115,17 @@ class Cart extends Model {
 	public function getProducts(){
 
 		$sql = new Sql();
+
 		$rows = $sql->select("
-			SELECT b.idproduct, b.desproduct , b.vlprice, b.vlwidth, b.vlheight, b.vllength, b.vlweight, b.desurl, COUNT(*) AS nrqtd, SUM(b.vlprice) AS vltotal 
+			SELECT b.idproduct, b.desproduct , b.vlprice, b.vlwidth, b.vlheight, b.vllength, b.vlweight, b.desurl,
+			COUNT(*) AS nrqtd, SUM(b.vlprice) AS vltotal 
 			FROM tb_cartsproducts a 
 			INNER JOIN tb_products b ON a.idproduct = b.idproduct 
 			WHERE a.idcart = :idcart AND a.dtremoved IS NULL 
 			GROUP BY b.idproduct, b.desproduct , b.vlprice, b.vlwidth, b.vlheight, b.vllength, b.vlweight, b.desurl 
 			ORDER BY b.desproduct
-		", [
-			':idcart'=>$this->getidcart()
-		]);
+		", [':idcart'=>$this->getidcart()]);
+
 		return Product::checkList($rows);
     }
     
